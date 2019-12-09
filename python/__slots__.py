dbf1cfebbd28b626659322ebb826ddb27d3832f8
jihoon <blue_jihoon@naver.com>
2019-12-09 18:53:46 +0900

    update README.md

--- python/__slots__.py
+++ python/__slots__.py
@@ -1,5 +1,6 @@
 """
 여러개의 속성을 포함한 많은 수의 객체를 생성할 때 __slots__을 이용하여 메모리 소모를 줄일 수 있다.
+https://www.notion.so/afmadadans/Chapter-9-5549dcf350224b7285197067dda875de#095c1cd6ed0943e098d02db41f25a2e6
 """
 
 
