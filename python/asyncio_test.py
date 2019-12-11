081a03112cac2e2e00a22496a1aa8d3039a3ed89
jihoon <blue_jihoon@naver.com>
2019-12-11 22:35:06 +0900

    add asyncio_test.py to compare with ensure_future adn run_until_complete

---
+++ python/asyncio_test.py
@@ -0,0 +1,33 @@
+import asyncio
+import random
+
+"""
+목적에 따라 아래의 두 함수를 선택해 결과 출력
+ - run_until_complete: 실행 완료에 상관없이 실행된 순서로 결과값을 가져온다.
+ - ensure_future: 실행이 완료된 순서로 결과값을 가져온다. 
+"""
+
+
+async def network_request(number):
+    integer = random.randint(1, 3)
+    await asyncio.sleep(integer)
+    return {"success": True, "result": number ** 2}
+
+
+async def fetch_square(number):
+    response = await network_request(number)
+    if response["success"]:
+        print("Result is: {}".format(response["result"]))
+
+
+loop = asyncio.get_event_loop()
+# 순차 실행
+loop.run_until_complete(fetch_square(2))
+loop.run_until_complete(fetch_square(3))
+loop.run_until_complete(fetch_square(4))
+
+# # 연산이 완료되는대로 실행
+# asyncio.ensure_future(fetch_square(2))
+# asyncio.ensure_future(fetch_square(3))
+# asyncio.ensure_future(fetch_square(4))
+# loop.run_forever()
