70a3766e546719d1ae3d64e2d07315204773fed5
jihoon <blue_jihoon@naver.com>
2020-01-11 02:59:18 +0900

    Update asyncio_test.py

--- python/asyncio_test.py
+++ python/asyncio_test.py
@@ -10,7 +10,7 @@ import random
 
 async def network_request(number):
     integer = random.randint(1, 3)
-    await asyncio.sleep(integer)
+    await asyncio.sleep(integer)  # sleep 시간만큼 통신이 이루어진다고 가정
     return {"success": True, "result": number ** 2}
 
 
@@ -21,12 +21,12 @@ async def fetch_square(number):
 
 
 loop = asyncio.get_event_loop()
-# 순차 실행
+# 순서대로 실행 결과 출력
 loop.run_until_complete(fetch_square(2))
 loop.run_until_complete(fetch_square(3))
 loop.run_until_complete(fetch_square(4))
 
-# # 연산이 완료되는대로 실행
+# # 연산이 완료되는대로 실행결과 출력
 # asyncio.ensure_future(fetch_square(2))
 # asyncio.ensure_future(fetch_square(3))
 # asyncio.ensure_future(fetch_square(4))
